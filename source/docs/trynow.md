71e5225ce3876402ebcc92bba13ce6e76623244a
Robbie <akotoro22@gmail.com>
2019-01-24 13:59:26 +0000

    Content added

--- source/docs/trynow.md
+++ source/docs/trynow.md
@@ -10,3 +10,32 @@ Try our APIs and Webhooks now.
 
 1. **Configure your Webhooks** to receive webhook notifications through the specified endpoint. Example: **https://graph.epaygh.com**
 2. **Connect your payment page** so you can take payments from your customers directly into your Epay wallet.
+```
+<code class="language-html"> <strong>HTML</strong>
+    &lt;a href="https://epaygh.com/pay/st-event-demo-app" class="btn btn-primary"&gt;pay&lt;/a&gt;
+</code>
+````
+3. **Receive and Handle Webhook Notifications** to confirm payment transactions and fulfill purchases
+```
+{
+    "success": true,
+    "transaction": {
+        "reference": "EP-XSks6NGSvtd",
+        "payment_method": "momo",
+        "amount": "1",
+        "status": "success",
+        "description": "Payment for sample event",
+        "customer": {
+            "name": "sam",
+            "email": "sam@gmail.com",
+            "telephone": "054********"
+        },
+        "page": {
+            "id": 73,
+            "name": "Event Demo App",
+            "description": "A demo payment page ",
+            "slug": "st-event-demo-app"
+        }
+    }
+}
+```
--- source/trynow.blade.php
+++ source/trynow.blade.php
@@ -5,19 +5,45 @@
 <section class="container max-w-2xl mx-auto px-4 py-6 md:py-8 ">
     <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
         <div class="mt-2">
-            <h1 id="Epay Docs">Try Now</h1>
+            <h1>Try Now</h1>
+<p class="text-lg">Get up and running with our webhook driven service and start your integration. Integrating Epay into your app or website can begin as soon as you <a href="https://epaygh.com/register">Create an Account, </a>requiring only three steps;</p>
+<ol>
+    <li><strong>Configure your Webhooks</strong> to receive webhook notifications through the specified endpoint. Example: <strong><a href="https://graph.epaygh.com">https://graph.epaygh.com</a></strong></li>
+    <li><strong>Connect your payment page</strong> so you can take payments from your customers directly into your Epay wallet.</li>
+        <pre>
+<code class="language-html"> <strong>HTML</strong>
+    &lt;a href="https://epaygh.com/pay/st-event-demo-app" class="btn btn-primary"&gt;pay&lt;/a&gt;
+</code>
+</pre>
+        <li>Receive and Handle Webhook Notifications to confirm payment transactions and fulfill purchases</li><p>
+<pre>
+<code class="language-json">
+<strong>JSON</strong>
+    {
+    "success": true,
+    "transaction": {
+        "reference": "EP-XSks6NGSvtd",
+        "payment_method": "momo",
+        "amount": "1",
+        "status": "success",
+        "description": "Payment for sample event",
+        "customer": {
+            "name": "sam",
+            "email": "sam@gmail.com",
+            "telephone": "054********"
+        },
+        "page": {
+            "id": 73,
+            "name": "Event Demo App",
+            "description": "A demo payment page ",
+            "slug": "st-event-demo-app"
+        }
+    }
+}
+</code>
+</pre>
 
-            <h4 class="font-light mt-4">Try Our APIs or Webhooks now.</h4>
-                
-                <ol>
-                    <li>
-                        <p><strong class="text-blue-dark">Configure your Webhooks</strong> to receive webhook notifications through the specified endpoint. Example: <strong>https://graph.epaygh.com</strong>
-                    </li>
-                    <li>
-                        <p><strong class="text-blue-dark">Connect your payment page</strong> so you can take payments from your customers directly into your Epay wallet
-                    </li>
-                </ol>
-               
+</ol>        
         </div>
     </section>
 
