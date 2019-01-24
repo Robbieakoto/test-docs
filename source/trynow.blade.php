e83e1289f25a5b86a98284ebed91bda2fb3d5343
Robbie <akotoro22@gmail.com>
2019-01-24 09:46:58 +0000

    Try now page added

--- source/trynow.blade.php
+++ source/trynow.blade.php
@@ -2,4 +2,23 @@
 
 @section('body')
 
+<section class="container max-w-2xl mx-auto px-4 py-6 md:py-8 ">
+    <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
+        <div class="mt-2">
+            <h1 id="Epay Docs">Try Now</h1>
+
+            <h4 class="font-light mt-4">Try Our APIs or Webhooks now.</h4>
+                
+                <ol>
+                    <li>
+                        <p><strong class="text-blue-dark">Configure your Webhooks</strong> to receive webhook notifications through the specified endpoint. Example: <strong>https://graph.epaygh.com</strong>
+                    </li>
+                    <li>
+                        <p><strong class="text-blue-dark">Connect your payment page</strong> so you can take payments from your customers directly into your Epay wallet
+                    </li>
+                </ol>
+               
+        </div>
+    </section>
+
 @endsection
